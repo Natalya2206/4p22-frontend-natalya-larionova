50166dc8e92b1c16cd170ebbc886bffc72bded95
Natalya <salander2015@mail.ru>
2022-10-27 15:08:28 +0300

    create homework_8

---
+++ homework_8/script.js
@@ -0,0 +1,42 @@
+'use strict';
+
+const button = document.getElementById('button');
+
+button.addEventListener('click', (event) => {
+    event.preventDefault();
+    const firstNumber = document.getElementById('first-number').value;
+    const symbol = document.getElementById('symbol').value;
+    const secondNumber = document.getElementById('second-number').value;
+    let result;
+
+
+    switch (symbol) {
+        case '': result = 'Не введён знак';
+        break;
+        case '+': result = Number(firstNumber) + Number(secondNumber);
+        break;
+        case '-': result = Number(firstNumber) - Number(secondNumber);
+        break;
+        case '*': result = Number(firstNumber) * Number(secondNumber);
+        break;
+        case '/': result = Number(firstNumber) / Number(secondNumber);
+        break;
+        default: result = 'Программа не поддерживает такую операцию';
+        break;
+    };
+
+    if (firstNumber === '') {
+        result = 'Первое число не указано';
+    } else if (secondNumber === '') {
+        result = 'Второе число не указано';
+    } else if (isNaN(firstNumber) === true || isNaN(secondNumber) === true) {
+        result = 'Некорректный ввод чисел';
+    } else if (result === Infinity) {
+        result = 'Операция некорректна';
+    } else {
+        console.log('Всё верно');
+    }
+
+    document.getElementById('result').innerHTML = result;
+    console.log(result);
+});    
